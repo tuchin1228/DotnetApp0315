f286406d8b441f62b82f50ca3d9ebad3b0d0bd97
tuchin1228 <jhinwei1228@gmail.com>
2023-03-15 14:16:18 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DontetApp0315.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
